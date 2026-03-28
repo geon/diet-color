182e4e3b19e9515bacbc98a3175a234c0608fc52
Victor Widell <victor.widell@gmail.com>
2026-03-28 14:36:34 +0100

    Set up Prettier.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './ui/App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./ui/App.jsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
